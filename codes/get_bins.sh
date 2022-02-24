4c94048e42f5eda0e9e27c6d51dbfe0c9737bbed
Juan M. Cebrian <jm.cebriangonzalez@gmail.com>
2022-02-24 18:02:46 +0100

    Initial commit, includes binding, hooks markings and reset stats, missing atomic barriers before reset stats.

---
+++ codes/get_bins.sh
@@ -0,0 +1,14 @@
+cp ./apps/barnes/BARNES ./bins/BARNES
+cp ./kernels/cholesky/CHOLESKY ./bins/CHOLESKY
+cp ./kernels/fft/FFT ./bins/FFT
+cp ./apps/fmm/FMM ./bins/FMM
+cp ./kernels/lu/contiguous_blocks/LU ./bins/LU-CONT
+cp ./kernels/lu/non_contiguous_blocks/LU ./bins/LU-NOCONT
+cp ./apps/ocean/contiguous_partitions/OCEAN ./bins/OCEAN-CONT
+cp ./apps/ocean/non_contiguous_partitions/OCEAN ./bins/OCEAN-NOCONT
+cp ./apps/radiosity/RADIOSITY ./bins/RADIOSITY
+cp ./kernels/radix/RADIX ./bins/RADIX
+cp ./apps/raytrace/RAYTRACE ./bins/RAYTRACE
+cp ./apps/volrend/VOLREND ./bins/VOLREND
+cp ./apps/water-nsquared/WATER-NSQUARED ./bins/WATER-NSQUARED
+cp ./apps/water-spatial/WATER-SPATIAL ./bins/WATER-SPATIAL
